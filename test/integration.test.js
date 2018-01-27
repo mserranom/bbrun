f58e299ab520d9b3d640964be2a632a0029bb66a
miguel <miguel@productml.com>
2018-01-27 18:54:04 +0000

    fixed test

--- test/integration.test.js
+++ test/integration.test.js
@@ -69,6 +69,14 @@ describe("template with multiple steps in the default pipeline", () => {
     expect(res.stderr).toBe("");
     expect(res.code).toBe(0);
   });
+  it("should execute a single step by name with spaces in the name", () => {
+    const res = run(
+      '"Step Three" --template test/templates/default-pipeline-multiple-steps.yml --dry-run'
+    );
+    // expect(norm(res.stdout)).toMatchSnapshot();
+    expect(res.stderr).toBe("");
+    expect(res.code).toBe(0);
+  });
 });
 
 it("template with no default should fail when no step name is passed", () => {
