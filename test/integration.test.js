62112c4e5f0c3296c71c63d7e0ce60ea276222e5
miguel <miguel@productml.com>
2018-01-27 18:28:00 +0000

    added test coverage for execution of multiple steps

--- test/integration.test.js
+++ test/integration.test.js
@@ -51,3 +51,22 @@ it("no image template should use default atlassian image", () => {
   expect(res.stderr).toBe("");
   expect(res.code).toBe(0);
 });
+
+describe("template with multiple steps in the default pipeline", () => {
+  it("should execute all the default steps when no argument is provided", () => {
+    const res = run(
+      "--template test/templates/default-pipeline-multiple-steps.yml --dry-run"
+    );
+    expect(norm(res.stdout)).toMatchSnapshot();
+    expect(res.stderr).toBe("");
+    expect(res.code).toBe(0);
+  });
+  it("should execute a single step by name", () => {
+    const res = run(
+      "step2 --template test/templates/default-pipeline-multiple-steps.yml --dry-run"
+    );
+    expect(norm(res.stdout)).toMatchSnapshot();
+    expect(res.stderr).toBe("");
+    expect(res.code).toBe(0);
+  });
+});
