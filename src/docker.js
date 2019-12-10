5e1d766d6607669340cf09320e21053e5f5cea50
Luciano Santana <contato@lucianosantana.net>
2019-12-10 12:05:08 +0100

    Use sh instead of bash as default (#40)

--- src/docker.js
+++ src/docker.js
@@ -21,7 +21,7 @@ function deleteBuildScript() {
 
 function prepareBuildScript(commands) {
   deleteBuildScript();
-  const script = "#!/usr/bin/env bash\n" + commands.join("\n");
+  const script = "#!/usr/bin/env sh\n" + commands.join("\n");
   fs.writeFileSync(BUILD_SCRIPT, script);
 }
 
@@ -49,8 +49,8 @@ function run(commands, image, dryRun, interactive, workDir, ignoreFolder) {
   }
 
   const cmd = interactive
-    ? `run --rm -P -it --entrypoint=/bin/bash -v ${pwd()}:${workDir} ${ignore} -w ${workDir} ${image}`
-    : `run --rm -P --entrypoint="" --net=host -v ${pwd()}:${workDir} ${ignore} -w ${workDir} ${image} bash ${BUILD_SCRIPT}`;
+    ? `run --rm -P -it --entrypoint=/bin/bash -v ${pwd()}:${workDir} -w ${workDir} ${image}`
+    : `run --rm -P -v ${pwd()}:${workDir} -w ${workDir} ${image} bash ${BUILD_SCRIPT}`;
 
   if (dryRun) {
     console.log(`docker command:\n\tdocker ${cmd}`);
